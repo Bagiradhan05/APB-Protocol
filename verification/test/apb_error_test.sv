1c1f2532334daae689fbf764552434ca3abc25e7
BAGIRADHAN S <bagiradhansrinivasan@gmail.com>
2026-06-08 21:52:32 +0530

    Add files via upload

---
+++ verification/test/apb_error_test.sv
@@ -0,0 +1,34 @@
+
+//***************************************//
+//
+// Author     :BAGIRADHAN S
+// e-mail     :bagiradhansrinivasan@gmail.com
+// Project    :APB protocol
+// Description:This file is implements the apb test
+// Date       :04/03/2026
+//
+//***************************************//
+
+`ifndef _APB_ERROR_TEST
+`define _APB_ERROR_TEST
+
+class apb_error_test;
+   
+   apb_env env;
+   virtual apb_interface vif;
+
+   function new(virtual apb_interface vif);
+      this.vif=vif;
+   endfunction:new
+
+   task run();
+      env=new(vif);
+      env.agent_h.gen.count=5;
+      env.agent_h.gen.error=1;
+      env.agent_h.gen.mode=1;
+      env.run();
+   endtask:run
+
+endclass:apb_error_test
+
+`endif
---
+++ verification/test/apb_read_test.sv
@@ -0,0 +1,33 @@
+//***************************************//
+//
+// Author     :BAGIRADHAN S
+// e-mail     :bagiradhansrinivasan@gmail.com
+// Project    :APB protocol
+// Description:This file is implements the apb test
+// Date       :04/03/2026
+//
+//***************************************//
+
+`ifndef _APB_READ_TEST
+`define _APB_READ_TEST
+
+class apb_read_test;
+   
+   apb_env env;
+   virtual apb_interface vif;
+
+   function new(virtual apb_interface vif);
+      this.vif=vif;
+   endfunction:new
+
+   task run();
+      env=new(vif);
+      env.agent_h.gen.count=10;
+      env.agent_h.gen.write=0;
+      env.agent_h.gen.error=0;
+      env.run();
+   endtask:run
+
+endclass:apb_read_test
+
+`endif
