e3d0b6b65893468b13ac2332306831e0d6df2a32
BAGIRADHAN S <bagiradhansrinivasan@gmail.com>
2026-06-08 21:53:59 +0530

    Update apb_test.sv

--- verification/test/apb_test.sv
+++ verification/test/apb_test.sv
@@ -3,7 +3,7 @@
 // Author     :BAGIRADHAN S
 // e-mail     :bagiradhansrinivasan@gmail.com
 // Project    :APB protocol
-// Description:This file is implements the apb test
+// Description:This file is implements the apb read followed by write test
 // Date       :04/03/2026
 //
 //***************************************//
